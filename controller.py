94caa8d4cd4fe439a71ad3c5ca83ec2ae749cc9c
Misirlou <diogotbasto@gmail.com>
2013-12-02 17:51:44 +0000

    colocar as camaras

--- controller.py
+++ controller.py
@@ -9,20 +9,36 @@ def is_mouse_visible(semantic_camera_stream):
 		if visible_object['name'] == "MOUSE":
 			return True
 	return False
+	
+def where_is(agentPose_stream):
+	pose = agentPose_stream.get()
+	return pose
+	
 
 def main():
 	""" Use the semantic cameras to locate the target and follow it """
 	with Morse() as morse:
 		semanticL = morse.cat.semanticL
 		#semanticR = morse.cat.semanticR
-		motion = morse.cat.stabilizedquadrotor
-
+		pose = morse.cat.pose
+		motion = morse.cat.waypoint
+		
 		while True:
 			mouse_seen_left = is_mouse_visible(semanticL)
 			#mouse_seen_right = is_mouse_visible(semanticR)
 			#pitch,roll,yaw,height
-			mot = {"theta_c":0,"phi_c":0,"psi_c":0,"h_c":15}
-			motion.publish(mot)
+			#mot = {"theta_c":0,"phi_c":0,"psi_c":0,"h_c":15}
+			#motion.publish(mot)
+			pos=where_is(pose)
+			waypoint = {	"x": 5, \
+								"y": 5, \
+								"z": 2, \
+								"yaw": 0, \
+								"tolerance": 0.5 \
+							}
+			print(pos)
+			#motion.publish(waypoint)
+			
 			
 
 if __name__ == "__main__":
--- scene.py
+++ scene.py
@@ -17,23 +17,44 @@ cat = Quadrotor()
 cat.translate(x=-7.0, z=1.0)
 cat.rotate(z=pi/3)
 
+semanticF = SemanticCamera()
+semanticF.translate(x=0.3, z=-0.08)
+semanticF.rotate(y=+pi/4)
+semanticF.properties(Vertical_Flip=False)
+semanticF.add_stream('socket')
+cat.append(semanticF)
+
 semanticL = SemanticCamera()
-semanticL.translate(x=0.3, z=-0.05)
-semanticL.rotate(x=+0.2)
+semanticL.translate(y=0.3, z=-0.08)
+semanticL.rotate(x=+pi/3,y=+pi/4)
 semanticL.properties(Vertical_Flip=False)
 semanticL.add_stream('socket')
 cat.append(semanticL)
+
+semanticR = SemanticCamera()
+semanticR.translate(y=-0.3, z=-0.08)
+semanticR.rotate(x=-pi/3,y=+pi/4)
+semanticR.properties(Vertical_Flip=False)
+semanticR.add_stream('socket')
+cat.append(semanticR)
 
 #stabilizedquadrotor = StabilizedQuadrotor()
 #stabilizedquadrotor.translate(0, 0, 0.1)
 #cat.append(stabilizedquadrotor)
 #stabilizedquadrotor.add_interface('socket')
 
+waypoint = RotorcraftWaypoint()
+cat.append(waypoint)
+waypoint.add_stream('socket')
 
+pose = Pose()
+cat.append(pose)
+pose.add_stream('socket')
 
 
 #environment
 env = Environment('land-1/trees')
 env.place_camera([10.0, -10.0, 10.0])
 env.aim_camera([1.0470, 0, 0.7854])
-env.select_display_camera(semanticL)
+
+env.select_display_camera(semanticF)
