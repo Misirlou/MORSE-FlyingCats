e0ba04b11bfd745b2e252596b1fcbf2ff15fbfba
João Costa <jdpc557@gmail.com>
2013-12-03 18:44:17 +0000

    Following based on camera

--- controller.py
+++ controller.py
@@ -6,13 +6,13 @@ dist=1.5
 height=5
 
 def is_mouse_visible(semantic_camera_stream):
-	""" Read data from th	e semantic camera, and determine if a specific
+	""" Read data from the semantic camera, and determine if a specific
 	object is within the field of view of the robot """
 	data = semantic_camera_stream.get()
 	visible_objects = data['visible_objects']
 	for visible_object in visible_objects:
 		if visible_object['name'] == "MOUSE":
-			return True
+			return {"x":visible_object['position'][0],"y":visible_object['position'][1],"z":visible_object['position'][2]}
 	return False
 	
 def where_is(agentPose_stream):
@@ -31,27 +31,49 @@ def main():
 		pose = morse.cat.pose
 		motion = morse.cat.waypoint
 		#motion=morse.cat.catmotion
+		pos=where_is(pose)
+		waypoint={ "x":pos['x'],"y":pos['y'],"z":height,"yaw":pos['yaw'],"tolerance":0.5}
 		
 		while True:
 			mouse_seen_left = is_mouse_visible(semanticL)
-			mouse_seen_right = is_mouse_visible(semanticF)
-			mouse_seen_front = is_mouse_visible(semanticR)
+			mouse_seen_right = is_mouse_visible(semanticR)
+			mouse_seen_front = is_mouse_visible(semanticF)
 			#mouse_seen_right = is_mouse_visible(semanticR)
 			#pitch,roll,yaw,height
 			#mot = {"theta_c":0,"phi_c":0,"psi_c":0,"h_c":15}
 			#motion.publish(mot)
 			pos=where_is(pose)
-			waypoint={ "x":pos['x']+dist*math.cos(pos['yaw']),"y":pos['y']+dist*math.sin(pos['yaw']),"z":height,"yaw":pos['yaw'],"tolerance":0.5}
+			#waypoint={ "x":pos['x']+dist*math.cos(pos['yaw']),"y":pos['y']+dist*math.sin(pos['yaw']),"z":height,"yaw":pos['yaw'],"tolerance":0.5}
 			
 			if mouse_seen_front:
-				waypoint={ "x":pos['x']+dist*math.cos(pos['yaw']),"y":pos['y']+dist*math.sin(pos['yaw']),"z":height,"yaw":pos['yaw'],"tolerance":0.5}
+				#waypoint=waypoint;
+				#print(mouse_seen_front['x'])
+				#waypoint={
+				#	"x":pos['x']+mouse_seen_front['z']*math.cos(pos['yaw']),
+				#	"y":pos['y']+mouse_seen_front['z']*math.sin(pos['yaw']),
+				#	"z":height,
+				#	"yaw":pos['yaw'],
+				#	"tolerance":0.5}
+				waypoint={
+					"x":mouse_seen_front['x'],
+					"y":mouse_seen_front['y'],
+					"z":height,
+					"yaw":pos['yaw'],
+					"tolerance":0.5}
 			elif mouse_seen_right:
-				rotate=pos['yaw']-math.pi/6
-				waypoint={ "x":pos['x']+dist*math.cos(pos['yaw']),"y":pos['y']+dist*math.sin(pos['yaw']),"z":height,"yaw":rotate,"tolerance":0.5}
+				waypoint={
+					"x":mouse_seen_right['x'],
+					"y":mouse_seen_right['y'],
+					"z":height,
+					"yaw":pos['yaw']-math.pi/3,
+					"tolerance":0.5}
 			elif mouse_seen_left:
-				rotate=pos['yaw']+math.pi/6
-				waypoint={ "x":pos['x']+dist*math.cos(pos['yaw']),"y":pos['y']+dist*math.sin(pos['yaw']),"z":height,"yaw":rotate,"tolerance":0.5}
-			print(pos['yaw'])
+				waypoint={
+					"x":mouse_seen_left['x'],
+					"y":mouse_seen_left['y'],
+					"z":height,
+					"yaw":pos['yaw']+math.pi/3,
+					"tolerance":0.5}
 			motion.publish(waypoint)
 			#motion.publish({"roll":0,"pitch":0.2,"yaw":0.1,"thrust":2})
 			
--- scene.py
+++ scene.py
@@ -19,21 +19,21 @@ cat.rotate(z=pi/3)
 
 semanticF = SemanticCamera()
 semanticF.translate(x=0.3, z=-0.08)
-semanticF.rotate(y=+pi/6)
+semanticF.rotate(y=-pi/6)
 semanticF.properties(Vertical_Flip=False)
 semanticF.add_stream('socket')
 cat.append(semanticF)
 
 semanticL = SemanticCamera()
 semanticL.translate(y=0.3, z=-0.08)
-semanticL.rotate(x=+pi/3,y=+pi/6)
+semanticL.rotate(x=+pi/3,y=-pi/6)
 semanticL.properties(Vertical_Flip=False)
 semanticL.add_stream('socket')
 cat.append(semanticL)
 
 semanticR = SemanticCamera()
 semanticR.translate(y=-0.3, z=-0.08)
-semanticR.rotate(x=-pi/3,y=+pi/6)
+semanticR.rotate(x=-pi/3,y=-pi/6)
 semanticR.properties(Vertical_Flip=False)
 semanticR.add_stream('socket')
 cat.append(semanticR)
@@ -53,8 +53,11 @@ pose.add_stream('socket')
 
 
 #environment
-env = Environment('land-1/trees')
+#env = Environment('land-1/trees')
+#env.place_camera([10.0, -10.0, 10.0])
+#env.aim_camera([1.0470, 0, 0.7854])
+env = Environment('sandbox', fastmode = False)
 env.place_camera([10.0, -10.0, 10.0])
-env.aim_camera([1.0470, 0, 0.7854])
+env.aim_camera([1.05, 0, 0.78])
 
 env.select_display_camera(semanticF)
