cb089ecbca01a580b1732687fe0a9b27cdff606e
Misirlou <diogotbasto@gmail.com>
2013-12-18 18:02:31 +0000

    coisas

--- runner.py
+++ runner.py
@@ -16,8 +16,7 @@ RANGE_TH=3
 
 def readSonar(sonar_stream):
 	sonar=sonar_stream.get()
-	pprint(sonar)
-	return sonar.range_list;
+	return sonar["range_list"];
 
 
 class KnowledgeBase:
@@ -25,23 +24,22 @@ class KnowledgeBase:
 		self.sonar=sonar
 		self.dir={"v":2,"w":0}
 	def updatePos(self):
-		self.sonar = readSonar(self.sonar)
-	def updateCarPos(self): # Updates the car position
-		print
-		if self.sonar[3]<RANGE_TH:
+		self.sonarArr = readSonar(self.sonar)
+	def updateDir(self): # Updates the car position
+		if self.sonarArr[3]<RANGE_TH:
 			self.dir={"v":V_FULLROTATE,"w":W_RIGHT};
-			print("front");
-		elif self.sonar[0]<RANGE_TH:
+			#self.dir={"v":0,"w":0};
+			print("front obstacle");
+		elif self.sonarArr[6]<RANGE_TH:
 			self.dir={"v":V_ROTATE,"w":W_RIGHT};
-			print("right");
-		elif self.sonar[6]<RANGE_TH:
+			print("left obstacle");
+		elif self.sonarArr[0]<RANGE_TH:
 			self.dir={"v":V_FULLROTATE,"w":W_LEFT};
-			print("left");
+			print("right obstacle");
 		else:
 			self.dir={"v":2,"w":0};
 			print("nothing");
 	def getDir(self):
-		
 		return self.dir
 
 
@@ -54,8 +52,8 @@ def main():
 		
 		while True:
 			kb.updatePos()
-			kb.updateCarDir();
-			motion.publish(kb.getDirection)
+			kb.updateDir()
+			motion.publish(kb.getDir())
 			
 
 if __name__ == "__main__":
--- scene.py
+++ scene.py
@@ -5,8 +5,10 @@ from math import pi
 mouse=ATRV()
 mouse.properties(Object = True, Graspable = False, Label = "MOUSE")
 mouse.translate(x=1.0, z=0.2)
+mouse.rotate(z=pi)
 
 motion = MotionVW()
+motion.add_stream('socket')
 mouse.append(motion)
 
 sick = Sick()
@@ -73,7 +75,7 @@ pose.add_stream('socket')
 #env = Environment('land-1/trees')
 #env.place_camera([10.0, -10.0, 10.0])
 #env.aim_camera([1.0470, 0, 0.7854])
-env = Environment('sandbox', fastmode = False)
+env = Environment('./indoor-1.blend', fastmode = False)
 env.place_camera([10.0, -10.0, 10.0])
 env.aim_camera([1.05, 0, 0.78])
 
