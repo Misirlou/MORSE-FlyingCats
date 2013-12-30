9bcdb04d9295879b4b5d722a72c4e0909d098fb1
Misirlou <diogotbasto@gmail.com>
2013-12-30 14:39:47 +0000

    algoritmo para andar em espirais

--- chaser.py
+++ chaser.py
@@ -7,11 +7,12 @@ from pymorse import Morse
 import math
 
 NEAR_DIST     = 1.5  # Landing Distance
-SAFETY_HEIGHT = 5    # Safety Height (Stays at this height until its near the car)
+SAFETY_HEIGHT = 4    # Safety Height (Stays at this height until its near the car)
 CAR_HEIGHT    = 0.9    # Height of the car
 WP_TOLERANCE  = 0.8    # Waypoint Tolerance
 LOST_TH	= 15
 LOST_TH_BOTTOM = 10
+LOST_DIST_TH = 20.0
 
 def isCarVisible(semantic_camera_stream):
 	""" Read data from the semantic camera, and determine if a specific
@@ -37,9 +38,14 @@ class KnowledgeBase:
 		self.semanticR=semanticR
 		self.semanticB=semanticB
 		#self.semanticF=semanticF
-		self.lost=0
+		self.resetLost()
 	def updatePos(self):
 		self.pos = whereIs(self.pose)
+	def resetLost(self):
+		self.lost=0
+		self.lost_dist=0.5
+		self.lost_dist_th=LOST_DIST_TH/6
+		self.lost_orientation=self.pos['yaw']
 	def updateCarPos(self): # Updates the car position
 		carSeenLeft  = isCarVisible(self.semanticL)
 		carSeenFront = isCarVisible(self.semanticB)
@@ -51,6 +57,7 @@ class KnowledgeBase:
 			self.goal["z"]   = carSeenFront["z"]+CAR_HEIGHT;
 			self.goal["yaw"] = self.pos["yaw"]
 			print("bottom");
+			self.resetLost()
 			self.lost=LOST_TH_BOTTOM
 		elif carSeenLeft and carSeenRight:
 			self.goal["x"]   = carSeenLeft["x"];
@@ -58,29 +65,37 @@ class KnowledgeBase:
 			self.goal["z"]   = carSeenLeft["z"]+CAR_HEIGHT;
 			self.goal["yaw"] = self.pos["yaw"]
 			print("front");
-			self.lost=0
+			self.resetLost()
 		elif carSeenLeft:
 			self.goal["x"]   = carSeenLeft["x"];
 			self.goal["y"]   = carSeenLeft["y"];
 			self.goal["z"]   = carSeenLeft["z"]+CAR_HEIGHT;
 			self.goal["yaw"] = self.pos["yaw"]+math.pi/8
 			print("left");
-			self.lost=0
+			self.resetLost()
 		elif carSeenRight:
 			self.goal["x"]   = carSeenRight["x"];
 			self.goal["y"]   = carSeenRight["y"];
 			self.goal["z"]   = carSeenRight["z"]+CAR_HEIGHT;
 			self.goal["yaw"] = self.pos["yaw"]-math.pi/8;
 			print("right");
-			self.lost=0
+			self.resetLost()
 		else:
 			if self.lost<LOST_TH:
 				self.lost+=1
 			else:
-				self.goal["x"]   = self.pos["x"];
-				self.goal["y"]   = self.pos["y"];
+				self.lost_dist+=1
+				if self.lost_dist>=self.lost_dist_th:
+					self.lost_dist=0.0
+					self.lost_dist_th+=2.0
+					if self.lost_dist_th>=LOST_DIST_TH:
+						self.lost_dist_th=LOST_DIST_TH/6
+					self.goal["yaw"] = self.pos["yaw"]+math.pi/2
+				else:
+					self.goal["yaw"] = self.pos["yaw"]
+				self.goal["x"]   = self.pos["x"]+math.cos(self.goal["yaw"])*self.lost_dist;
+				self.goal["y"]   = self.pos["y"]+math.sin(self.goal["yaw"])*self.lost_dist;
 				self.goal["z"]   = SAFETY_HEIGHT;
-				self.goal["yaw"] = self.pos["yaw"]+math.pi/3
 				print("lost");
 	def getWaypoint(self):
 		dist = math.sqrt((self.goal["x"]-self.pos["x"])**2+(self.goal["y"]-self.pos["y"])**2)
